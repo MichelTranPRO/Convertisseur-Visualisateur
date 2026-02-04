0c3b56afe80abd69bdfeebdf6626366f5e99bcff
EmmanuelTiamzon <emmanuelsrivastava6@gmail.com>
2026-02-04 16:30:25 +0100

    end of the class, starting it's javadoc

--- src/fr/iutfbleau/pif/Converter/ImageReader.java
+++ src/fr/iutfbleau/pif/Converter/ImageReader.java
@@ -1,5 +1,12 @@
+/* 
+ * La classe <code>ImageReader</code> est utilisée comme modèle dans un encadrement MVC. 
+ * Il fonctionne donc avec les données, reçu en ligne de commande en le lisant dans un format supporté par la méthode read de la classe ImageIO. 
+ * 
+ * @version 1.1
+ * @author Emmanuel SRIVASTAVA-TIAMZON, Michel TRAN, Rayan BISSON
+ */ 
+
 import javax.imageio.*;
-import javax.swing.*;
 import java.awt.image.*;
 import java.io.*;
 
@@ -8,7 +15,7 @@ public class ImageReader {
 	private File fileInput;
 	private BufferedImage image;
 
-	public ImgReader(File fileInput) {
+	public ImageReader(File fileInput) {
 		this.fileInput = fileInput;
 		readFileInput(this.fileInput);
 	}
@@ -30,27 +37,26 @@ public class ImageReader {
 		return this.fileInput;
 	}
 
-	public String toString() {
-		String aRenvoyer = "";
-		if(this.fileInput == null) {
-
-		} else {
-			this.image.getWidth();
-		}
-
-		return aRenvoyer;
+	public BufferedImage getImage() {
+		return this.image;
 	}
 
-	public verifyInputLength(String[] input) {
-		if(input.length > 3) {
-			
+	public void verifyInputLength(String[] input) {
+		if(input.length == 0) {
+			System.out.println("The given file is not in the correct format");
 		} else {
-			openJFileChooser();
+			//ici ça renverra vers l'ouverture JFileChooser en utilisant une autre classe.
 		}
 	}
 
 	public static void main(String[] args) {
-		File file = new File(args[0]);
-		ImageReader myImage = new ImageReader(file);
-	}
+		if(args.length > 0) {
+			File file = new File(args[0]);
+			ImageReader myImage = new ImageReader(file);
+			if(myImage.getImage() != null) {
+				System.out.println("The image was sucessfully loaded !")
+			}
+		} else {
+			System.out.println("No file found in argument.");
+		}
 }
