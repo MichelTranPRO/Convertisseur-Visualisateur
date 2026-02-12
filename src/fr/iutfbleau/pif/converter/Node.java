4532d85b56ccc67384032dd00728dca1160377ca
nossibR <rayanbisson44@gmail.com>
2026-02-12 08:02:27 +0100

    started implementation of the binary tree

---
+++ src/fr/iutfbleau/pif/converter/Node.java
@@ -0,0 +1,13 @@
+
+
+public class Node{
+  private int label;
+  private Node leftNode;
+  private Node rightNode;
+  public Node(int label){
+    this.label = label;
+    this.leftNode = null;
+    this.rightNode = null;
+  }
+}
+
