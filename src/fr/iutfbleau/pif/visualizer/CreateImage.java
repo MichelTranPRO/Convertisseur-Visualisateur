e0a3008f07f96f847fc609fdfe3ab7196f6d1c42
SRIVASTAVA TIAMZON Emmanuel <emmanuel.srivastava-tiamzon@vinci-construction.com>
2026-02-24 16:39:26 +0100

    javadoc for the CreateImage class

--- src/fr/iutfbleau/pif/visualizer/CreateImage.java
+++ src/fr/iutfbleau/pif/visualizer/CreateImage.java
@@ -6,16 +6,41 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
+/**
+ * The class <code>CreateImage</code> is responsible for creating an image from a data input stream that goes into it's parameters.
+ */
 public class CreateImage {
+
+    /**
+     * This is one of the attributes of the class, it has a DataInputStream type that is used to read the data of the image.
+     */
     private DataInputStream dataInput;
+    
+    /**
+     * This is an integer that is used to store the current byte that is being read from the data input stream.
+     */
     private int currentByte;
+
+    /**
+     * This is an integer that is used to store the index of the bit.
+     */
     private int indexBit;
+
+    /**
+     * This is the constructor of the class, it has a DataInputStream type parameter that is used to read the data of the image.
+     * @param dataInput is the data input stream that is used to read the data of the image.
+     */
     public CreateImage(DataInputStream dataInput){
         this.dataInput=dataInput;
         this.currentByte=0;
         this.indexBit=-1;
     }
 
+    /**
+     * The method <code>getBit</code> is used to read the bits from the data input stream. 
+     * @return an Integer that is the value of the bit that is being read from the data input stream.
+     * @throws IOException
+     */
     private int getBit() throws IOException{
             if (indexBit < 0){
                 currentByte = dataInput.readUnsignedByte();
@@ -27,6 +52,13 @@ public class CreateImage {
             return bit;
     }
 
+    /**
+     * The method <code>decodeValue</code> is used to decode the value of the color from the data input stream. 
+     * It uses a HashMap that is passed as a parameter to decode the value of the color.
+     * @param hash is a HashMap that is used to decode the value of the color, it has a String type as key and an Integer type as value.
+     * @return an Integer that is the value of the color that is being decoded.
+     * @throws IOException
+     */
     private int decodeValue(HashMap<String, Integer> hash) throws IOException{
         Integer intensity=null;
         String temp="";
@@ -43,7 +75,16 @@ public class CreateImage {
         return intensity;
     }
 
-
+    /**
+     * The method <code>buildImage</code> is used to build the image from the data input stream. It uses the method <code>decodeValue</code> to decode the value of the color and it uses the HashMaps that are passed as parameters to decode the value of the color.
+     * @param w
+     * @param h
+     * @param hashRed
+     * @param hashGreen
+     * @param hashBlue
+     * @return
+     * @throws IOException
+     */
     public BufferedImage buildImage(int w, int h, HashMap<String, Integer> hashRed
                                                 , HashMap<String, Integer> hashGreen
                                                 , HashMap<String, Integer> hashBlue) throws IOException {
