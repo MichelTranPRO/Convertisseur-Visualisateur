5f3d7b08739b8fe50a6353497a7ff449cd4aac6a
SRIVASTAVA TIAMZON Emmanuel <emmanuel.srivastava-tiamzon@vinci-construction.com>
2026-02-26 10:11:22 +0100

    update

--- src/fr/iutfbleau/pif/converter/CanonicalTables.java
+++ src/fr/iutfbleau/pif/converter/CanonicalTables.java
@@ -1,5 +1,7 @@
 package fr.iutfbleau.pif.converter;
 
+import java.util.HashMap;
+
 /**
  * La classe <code>CanonicalTables</code> contient la creation et la gestion complète des trois tables de codes canoniques
  *
@@ -7,5 +9,12 @@ package fr.iutfbleau.pif.converter;
  * @author Rayan Bisson, Michel Tran, Emmanuel Srivastava-Tiamzon
  */
 public class CanonicalTables{
-  private HashMap<String,  
+
+  /**
+   * Ces attributs sont les Hash Maps pour les données de couleur rouge, vert, et bleu, 
+   * elles sont utilisées pour stockées le code canonique en tant que clé et la valeur de l'intensité en tant que leur valeur respectives
+   */
+  private HashMap<String, Integer> hashRed;
+  private HashMap<String, Integer> hashGreen;
+  private HashMap<String, Integer> hashBlue;  
 }
