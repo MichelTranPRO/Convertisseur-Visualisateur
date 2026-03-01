1381cac37036ae9281aed2de71b9d5ca7c330d0b
srivasta <emmanuel.srivastava-tiamzon@etu-pec.fr>
2026-03-01 01:18:57 +0100

    BIG fix on the visualisation of the image and the table

--- src/fr/iutfbleau/pif/converter/ImageDrawer.java
+++ src/fr/iutfbleau/pif/converter/ImageDrawer.java
@@ -7,8 +7,8 @@ import javax.swing.JComponent;
 
 /**
  * La classe <code>ImageDrawer</code> est un composant graphique chargé de dessiner l'image.
- * Elle permet d'afficher l'image centrée ou avec un décalage spécifique généré par la souris.
- * @version 1.1
+ * Elle affiche l'image centrée et la réduit proportionnellement si elle dépasse du conteneur.
+ * @version 1.2
  * @author Rayan Bisson, Michel Tran, Emmanuel Srivastava-Tiamzon
  */
 public class ImageDrawer extends JComponent {
@@ -16,86 +16,56 @@ public class ImageDrawer extends JComponent {
     /** L'image source à dessiner. */
     private BufferedImage image;
 
-    /** Le décalage horizontal et vertical pour l'affichage de l'image. */
-    private int offsetX, offsetY;
-
     /**
      * Constructeur de la classe <code>ImageDrawer</code>.
      * @param image L'image à dessiner.
-     * @param offsetX Le décalage horizontal initial.
-     * @param offsetY Le décalage vertical initial.
      */
-    public ImageDrawer(BufferedImage image, int offsetX, int offsetY) {
+    public ImageDrawer(BufferedImage image) {
         this.image = image;
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
     }
 
-    /**
-     * Redéfinition de la méthode de dessin pour afficher l'image.
-     * Gère le centrage automatique si le conteneur est plus grand que l'image.
-     * @param pinceau L'objet Graphics utilisé pour dessiner.
-     */
     @Override
     public void paintComponent(Graphics pinceau) {
         Graphics secondPinceau = pinceau.create();
 
         if (this.image != null) {
-            //Test : secondPinceau.drawImage(this.image, this.offsetX, this.offsetY, this);
-            int drawX = this.offsetX;
-            int drawY = this.offsetY;
+            int panelWidth = this.getWidth();
+            int panelHeight = this.getHeight();
+            int imgWidth = this.image.getWidth();
+            int imgHeight = this.image.getHeight();
 
-            if(this.getWidth() > this.image.getWidth()) {
-                drawX = (this.getWidth() - this.image.getWidth()) / 2;
+            int drawWidth = imgWidth;
+            int drawHeight = imgHeight;
+
+            if (imgWidth > panelWidth || imgHeight > panelHeight) {
+                //ici on calcule le ratio pour pas se rater quand on rétrécit l'image
+                double widthRatio = (double) panelWidth / imgWidth;
+                double heightRatio = (double) panelHeight / imgHeight;
+                
+                // On prend le ratio le plus petit pour s'assurer que l'image rentre entièrement
+                double ratio = Math.min(widthRatio, heightRatio);
+
+                // On applique le ratio aux dimensions de dessin
+                drawWidth = (int) (imgWidth * ratio);
+                drawHeight = (int) (imgHeight * ratio);
             }
 
-            if(this.getHeight() > this.image.getHeight()) {
-                drawY = (this.getHeight() - this.image.getHeight()) / 2;
-            }
+            //on met l'image au milieu
+            int drawX = (panelWidth - drawWidth) / 2;
+            int drawY = (panelHeight - drawHeight) / 2;
 
-            secondPinceau.drawImage(this.image, drawX, drawY, this);
+            secondPinceau.drawImage(this.image, drawX, drawY, drawWidth, drawHeight, this);
         }
 
         secondPinceau.dispose();
     }
 
-    /**
-     * Setter pour les offset, il modifie les décalages de l'image et force le redessin du composant.
-     * @param offsetX Le nouveau décalage horizontal.
-     * @param offsetY Le nouveau décalage vertical.
-     */
-    public void setOffset(int offsetX, int offsetY) {
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
-        repaint();
-    }
-
-    /**
-     * Calcule et retourne la taille préférée du composant en fonction de l'image et des décalages.
-     * @return La dimension idéale pour afficher l'image complète.
-     */
     @Override
     public Dimension getPreferredSize() {
         if (this.image != null) {
-            return new Dimension(this.image.getWidth() + this.offsetX, this.image.getHeight() + this.offsetY);
+            return new Dimension(this.image.getWidth(), this.image.getHeight());
         } else {
             return super.getPreferredSize();
         }
-    }
-
-    /**
-     * Getter pour récupérer le décalage horizontal actuel.
-     * @return Le décalage sur l'axe X.
-     */
-    public int getOffsetX() {
-        return this.offsetX;
-    }
-
-    /**
-     * Getter pour récupérer le décalage vertical actuel.
-     * @return Le décalage sur l'axe Y.
-     */
-    public int getOffsetY() {
-        return this.offsetY;
     }
 }
--- src/fr/iutfbleau/pif/converter/ImagePanel.java
+++ src/fr/iutfbleau/pif/converter/ImagePanel.java
@@ -6,27 +6,19 @@ import java.awt.BufferedImage;
 
 /**
  * La classe <code>ImagePanel</code> représente un panneau d'affichage d'image dans l'interface graphique de l'application.
- * 
+ * @version 1.2
  */
 public class ImagePanel extends JPanel {
 	
 	/**
 	 * Constructeur de la classe <code>ImagePanel</code>.
-	 * Assemble le dessinateur d'image et le contrôleur de la souris dans le panneau.
 	 * @param image L'image à afficher et manipuler.
 	 */
 	public ImagePanel(BufferedImage image) {
 		BorderLayout borderLayout = new BorderLayout();
 		this.setLayout(borderLayout);
-
-		ImageDrawer imageDrawer = new ImageDrawer(image, 0, 0);
-		ImagePanelMouse mouseListener = new ImagePanelMouse(imageDrawer);
-
-		imageDrawer.addMouseListener(mouseListener);
-		imageDrawer.addMouseMotionListener(mouseListener);
-
+		ImageDrawer imageDrawer = new ImageDrawer(image);
 		this.add(imageDrawer, BorderLayout.CENTER);
-
 	}
 
     
